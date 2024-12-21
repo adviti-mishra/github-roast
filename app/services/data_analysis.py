ed3b2c8f3c074409f50cf66335c71c4f54aa2ee8
Adviti Mishra <adviti@umich.edu>
2024-12-22 05:23:35 +0530

    template functions for calculating metrics

---
+++ app/services/data_analysis.py
@@ -0,0 +1,10 @@
+
+# returns most ghosted user
+def calculate_most_ghosted(prs):
+    return "adviti_mishra"
+
+# returns longest period of inactivity
+
+
+def calculate_longest_period_of_inactivity(commits):
+    return "365"
