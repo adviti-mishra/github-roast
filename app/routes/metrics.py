7824415b20f7095ed496a7a0984014619c81a9d6
Adviti Mishra <adviti@umich.edu>
2024-12-22 04:44:40 +0530

    Templates for routes to return repo metrics

--- app/routes/metrics.py
+++ app/routes/metrics.py
@@ -2,3 +2,21 @@
 from fastapi import APIRouter
 
 router = APIRouter()
+
+# templates for routes
+
+# returns the most ghosted contributor
+
+
+@router.get("/most_ghosted")
+def most_ghosted():
+    # RIP @ adviti mishra
+    return {"contributor": "adviti_mishra"}
+
+# returns the length of the longest period without any commits
+
+
+@router.get("/longest_repo_inactivity")
+def longest_repo_inactivity():
+    # RIP again
+    return {"longest_repo_inactivity": "365"}
