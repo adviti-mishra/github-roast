4d00995939041d488bec3e599f79368d860a9fad
Adviti Mishra <adviti@umich.edu>
2024-12-22 05:10:43 +0530

    High-level route for returning all metrics. Individual routes for data dorks

--- app/routes/metrics.py
+++ app/routes/metrics.py
@@ -5,18 +5,36 @@ router = APIRouter()
 
 # templates for routes
 
+# Individual routes for data dorks
+
 # returns the most ghosted contributor
 
 
 @router.get("/most_ghosted")
 def most_ghosted():
+    # call the most_ghosted function
     # RIP @ adviti mishra
     return {"contributor": "adviti_mishra"}
 
 # returns the length of the longest period without any commits
 
 
-@router.get("/longest_repo_inactivity")
+@router.get("/longest_period_of_inactivity")
 def longest_repo_inactivity():
     # RIP again
-    return {"longest_repo_inactivity": "365"}
+    return {"longest_period_of_inactivity": "365"}
+
+# High-level route for web app
+
+# returns all the metrics
+
+
+@router.get("/metrics")
+def get_all_metrics():
+    # call the most_ghosted_user function
+    # call the longest_period_of_inactivity function
+    # return results as a single json object
+    return {
+        "most_ghosted": "adviti_mishra",
+        "longest_period_of_inactivity": "365"
+    }
